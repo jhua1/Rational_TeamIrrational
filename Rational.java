5382066226b1b463c8c9aa7417d9207fc7cfa42a
wuruochong <rwu3@stuy.edu>
2015-11-17 17:38:10 -0500

    prelim version of Rational, created Driver class

---
+++ Rational.java
@@ -0,0 +1,43 @@
+/*
+Team Irrational - James Hua, Ruochong Wu
+APCS1 pd10
+HW32 -- Irrationality Stops Here
+2015-11-17
+*/
+
+public class Rational{
+    private int Num;
+    private int Den;
+
+    public Rational{
+	Num = 0;
+	Den = 1;
+    }
+
+    public int getNum(){
+	return Num;
+    }
+
+    public int getDen(){
+	return Den;
+    }
+
+    public String toString(){
+	return Num + "/" + Den;
+    }
+
+    public double floatValue(){
+	return (double)Num/Den;
+    }
+
+    public void multiply(Rational x){
+	this.Num = this.Num * x.getNum();
+	this.Den = this.Den * x.getDen();
+    }
+
+    public void divide(Rational x){
+	this.Num = this.Num * x.getDen();
+	this.Den = this.Den * x.getNum();
+    }
+}
+	
